db882094fbccdd4ab8f4a382421c1260b5cfd148
Spencer Krum <nibz@cat.pdx.edu>
2014-10-12 18:02:44 -0700

    Better server

--- gfv-server.py
+++ gfv-server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import subprocess
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,8 +9,16 @@ def hello_world():
 
 @app.route('/video/<path:video>')
 def play_video(video):
-    subprocess.call(['gfv', video])
+    print "python", video
+#    try:
+#        os.remove('/tmp/gfv')
+#    except OSError:
+#        pass
+    #print ['~/get-flash-videos/get_flash_videos', '-f', '/tmp/gfv', '-p', '--player', '~/video.sh', str(video)]
+#    subprocess.call(['~/get-flash-videos/get_flash_videos', '-f', '/tmp/gfv', '-p', '--player', '~/video.sh', video], shell=True)
+#    subprocess.call(['/home/kiosk/sandbox/gfv-server/gfv-bin', video], shell=True)
+    os.system("/home/kiosk/sandbox/gfv-server/gfv-bin {0}".format(video))
     return "Playing Video {0}".format(video)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host='0.0.0.0')
